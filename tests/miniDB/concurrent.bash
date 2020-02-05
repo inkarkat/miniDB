69c62cc13fca103168aa4d1a96cc76f9355d825a
Ingo Karkat <swdev@ingo-karkat.de>
2020-02-05 14:12:32 +0100

    Tests: Minor: Only print the actual count in DEBUG mode

--- tests/miniDB/concurrent.bash
+++ tests/miniDB/concurrent.bash
@@ -11,14 +11,14 @@ INITIAL_ROW_NUM=2
 assert_counter()
 {
     total="$(miniDB --no-transaction --table "$BATS_TEST_NAME" --query counter --columns 1)"
-    echo >&3 "# total: $total"
+    [ "$DEBUG" ] && echo >&3 "# total: $total"
     [ $total "$@" ]
 }
 
 assert_key_num()
 {
     total="$(miniDB --no-transaction --table "$BATS_TEST_NAME" --query-keys | wc -l)"
-    echo >&3 "# total: $total"
+    [ "$DEBUG" ] && echo >&3 "# total: $total"
     [ $total "$@" ]
 }
 
