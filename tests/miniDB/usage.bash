4926093dede1a510b252bda71ab29c0227ee570a
Ingo Karkat <swdev@ingo-karkat.de>
2020-03-25 21:32:59 +0100

    Refactoring: Extract ACTIONS constant (in script and tests)

--- tests/miniDB/usage.bash
+++ tests/miniDB/usage.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+export ACTIONS='--update, --delete, --truncate, --drop, --[read-]command, --each, --query[-keys], --unescape, --start-read-transaction, --start-write-transaction, --upgrade-to-write-transaction, --within-transaction, --end-transaction, --abort-write-transaction'
 assert_multiple_actions_error()
 {
-    [ "${lines[0]}" = 'ERROR: Only one of --update, --delete, --truncate, --drop, --[read-]command, --each, --query[-keys], --unescape, --start-read-transaction, --start-write-transaction, --upgrade-to-write-transaction, --within-transaction, --end-transaction, --abort-write-transaction allowed.' ]
+    [ "${lines[0]}" = "ERROR: Only one of $ACTIONS allowed." ]
 }
